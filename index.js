353cd4ef89acb06f16626d1ec1c7d24302aedfaf
*Collins <nyamburycollins@mail.com>
2025-03-20 05:37:53 +0300

    Create event listeners on DOM nodes using addEventListener()

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+    // Get the element (button) to add the event listener to
+   const button = document.getElementById("button"); // Assuming you have a button with id="button"
+ 
+   // Add an event listener for the 'click' event
+   button.addEventListener("click", function () {
+     console.log("Button clicked!");
+   });
+ }
+
+
+
